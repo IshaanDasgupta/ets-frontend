97cfb9e0eb8654af86766f107422ae88cad9550c
anshjain16 <anshjain0216@gmail.com>
2024-10-11 03:45:11 +0530

    milan page 2.0

--- app/(mainFragments)/temp.tsx
+++ app/(mainFragments)/temp.tsx
@@ -2,48 +2,30 @@ import {
     StyleSheet,
     View,
     Alert,
+    ScrollView,
     TouchableOpacity,
     DrawerLayoutAndroid,
+    SafeAreaView,
     Image,
+    Pressable,
 } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import * as Location from "expo-location";
 import { useEffect, useState, useRef } from "react";
+import { Button, Slider, Text, TextField } from "react-native-ui-lib";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import socket from "../socket";
-import Entypo from "@expo/vector-icons/Entypo";
-import { Button, Slider, Text, TextField } from "react-native-ui-lib";
-import { ThemedTextField } from "@/components/ThemedTextField";
-import { ThemedButton } from "@/components/ThemedButton";
-import axios from "axios";
+import { AntDesign } from "@expo/vector-icons";
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 
 export default function HomeScreen() {
     const [initialRegion, setInitialRegion] = useState<any>(null);
     const [locationCoordinates, setLocationCoordinates] = useState<any>(null);
+    const [infoSize, setInfoSize] = useState<any>("min");
     const drawer = useRef<DrawerLayoutAndroid>(null);
 
-    const navigationView = () => (
-        <View style={styles.drawerContainer}>
-            <Text style={styles.header}>Service Provider Information</Text>
-
-            {/* <Image 
-            source={{ uri: person.image }} 
-            style={styles.image} 
-          /> */}
-
-            <View style={styles.infoContainer}>
-                <Text style={styles.paragraph}>First Name: Aditya</Text>
-                <Text style={styles.paragraph}>Last Name: Baghel</Text>
-                <Text style={styles.paragraph}>Phone Number: 7898261362</Text>
-            </View>
-
-            <Button
-                label="Close Info"
-                onPress={() => drawer.current?.closeDrawer()}
-            />
-        </View>
-    );
     useEffect(() => {
         checkIfLocationEnabled();
         getCurrentLocation();
@@ -94,33 +76,29 @@ export default function HomeScreen() {
         });
     };
 
+    function getInfoStyle(infoSize: any){
+        if(infoSize = "max"){
+            return {
+                position: 'absolute',
+                marginTop: "40%",
+                // backgroundColor: '#fff',
+                overflow: 'scroll',
+            }
+        }
+        else{
+            return {
+            }
+        }
+    }
+
     return (
         <ThemedView style={styles.container}>
-            <DrawerLayoutAndroid
-                ref={drawer}
-                drawerWidth={300}
-                drawerPosition="right"
-                renderNavigationView={navigationView}
-            >
-                <MapView
+            <MapView
                     style={styles.map}
                     initialRegion={initialRegion}
                     showsUserLocation
                     provider={PROVIDER_GOOGLE}
-                >
-                    {/* {helpAccepted && helperLocation ? (
-                            // <ThemedText>{helperLocation.latitude}</ThemedText>
-                            <Marker
-                                coordinate={{
-                                    latitude: helperLocation.latitude,
-                                    longitude: helperLocation.longitude,
-                                }}
-                                title="Helper"
-                                description="ishaan dasgupta"
-                            />
-                        ) : (
-                            <ThemedText>loading...</ThemedText>
-                        )} */}
+                    >
                     <Marker
                         coordinate={{
                             latitude: 22.731033,
@@ -128,28 +106,52 @@ export default function HomeScreen() {
                         }}
                         title="Helper"
                         description="aditya baghel"
-                    />
-                </MapView>
-                <View style={styles.container}>
-                    <Button
-                        label="Show Helper Information"
-                        onPress={() => drawer.current?.openDrawer()}
-                    />
-                </View>
-            </DrawerLayoutAndroid>
+                        />
+            </MapView>
+
+                <ThemedView style={infoSize == "min" ? styles.infoWrapperMin : styles.infoWrapperMax}>
+                        <Pressable style={styles.infoButton} onPress={() => setInfoSize((prevState : any) => prevState == "min" ? "max" : "min")}>
+                            {infoSize == "min" ? <AntDesign name="up" size={15} color="black" /> : <AntDesign name="down" size={15} color="black" />}
+                        </Pressable>
+                    {/* <Text style={styles.header}>Service Provider Information</Text> */}
+
+                    {/* <Image 
+                    source={{ uri: person.image }} 
+                    style={styles.image} 
+                    /> */}
+
+                    <ThemedView style={styles.infoContainer}>
+                        <ThemedView style={styles.infoMinWrapper}>
+                            <MaterialCommunityIcons name="face-man-profile" size={45} color="grey" />
+                            <ThemedView style={styles.infoMin}>
+                                <ThemedText style={styles.paragraph}>First Name: Aditya</ThemedText>
+                                <ThemedText style={styles.paragraph}>Last Name: Baghel</ThemedText>
+                                <ThemedText style={styles.paragraph}>Phone Number: 7898261362</ThemedText>
+                            </ThemedView>
+                            <SimpleLineIcons name="call-out" size={35} color="grey" />
+                        </ThemedView>
+                        <ThemedText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iusto, officia est atque hic delectus obcaecati, facere eveniet repellendus ipsa ex odit velit non, voluptatum dolorum reiciendis numquam. Laboriosam odit ex dicta quibusdam dolorem, suscipit ipsa quisquam ut. Animi optio doloribus saepe voluptatem numquam unde, aut ratione mollitia alias nisi tenetur velit tempore corrupti? Illum laborum impedit magnam, placeat accusamus odit quae ad ipsum et veniam cum consequatur eius. At provident, ullam vitae quam quod, cum aliquam a ex unde, nam autem nesciunt nostrum sapiente esse dolorem corporis pariatur inventore quasi. Rerum eum nihil temporibus repellendus fuga porro odio tenetur!</ThemedText>
+                        <ThemedText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum iste aspernatur eaque veniam molestiae. At delectus asperiores, nesciunt nulla labore quae numquam nostrum dicta aliquid exercitationem tempore ad aliquam veniam cumque? Molestias sapiente doloremque maiores possimus, libero rem labore quod non, facilis voluptatem vero accusantium aut ab architecto consequatur explicabo distinctio voluptates adipisci quisquam qui ducimus. Voluptas itaque nam tempora inventore provident assumenda at rerum obcaecati distinctio quidem ad nisi modi in, vero labore sunt veritatis ea reiciendis est porro corporis similique? Exercitationem molestias ipsa expedita voluptates. Magnam facilis incidunt ea sed est, quod aspernatur aliquam non adipisci, totam expedita minus dicta sunt provident. Amet ipsa saepe quaerat itaque eveniet. Velit quo molestias voluptatem, rerum veritatis nobis quae dolorem nisi obcaecati, eligendi quas modi facere laboriosam ad nostrum maiores ex tempore ipsum accusantium ducimus quidem in id beatae eius. Nemo cumque qui perspiciatis eos, corrupti perferendis hic neque consequuntur tempora, maxime pariatur. Ducimus excepturi aliquid dignissimos. Natus nemo ex deserunt magnam atque velit cum quod nisi mollitia voluptatibus reiciendis nihil deleniti dignissimos assumenda dolor perspiciatis, aut enim ea tenetur at. Error minus quae debitis tempore in reiciendis rem placeat! Assumenda explicabo ratione eius excepturi iusto quod id labore et, tempora vero corporis! Enim possimus, nobis perferendis, voluptas maxime quo eius accusantium minus quidem porro commodi ex aperiam omnis quia eum sapiente, deleniti tenetur dolorem natus nisi dignissimos quos harum officia architecto. Numquam est modi facilis iusto, voluptate et enim error nemo quam commodi explicabo sapiente aperiam quisquam aspernatur nulla repudiandae tempora asperiores accusamus ipsa voluptas quo velit. Fugit recusandae eos ratione, adipisci nisi odit distinctio placeat illum tempore, voluptatibus pariatur porro sequi perspiciatis nobis et quos, necessitatibus inventore. Excepturi, molestiae numquam sit consequatur, minus, expedita dolorem voluptatibus perferendis quidem minima quasi cum quae sed temporibus vel accusamus voluptatum corrupti. Consequuntur, porro ut quas perferendis deleniti exercitationem. Fugiat debitis ipsa beatae ad nobis earum. Quidem eius impedit repudiandae deleniti voluptate, porro est ratione atque aliquid veniam alias dignissimos illo earum? Ad similique explicabo neque ratione. Ipsam sint blanditiis repudiandae cupiditate odit libero sunt ex atque autem ratione distinctio explicabo doloribus eum, id accusamus at deleniti laboriosam repellendus dolorem. Tempore eveniet ipsa at fugit veritatis, velit sint, id quas, expedita omnis reprehenderit laborum. Illum fuga dolorem enim accusamus aliquid illo animi, mollitia nihil placeat quia ratione laudantium nemo laborum explicabo accusantium nobis commodi. Dolor, odio, molestiae quam natus porro amet atque enim laudantium repellat mollitia veniam suscipit nihil repellendus consequuntur, optio ducimus tenetur. Doloremque hic, temporibus fugiat quasi doloribus ratione veniam repudiandae quia nulla eos dolores repellendus quibusdam nobis at itaque libero nesciunt ipsum? Tempore sed expedita necessitatibus dicta non explicabo accusantium, cupiditate maxime, repellat voluptas voluptates doloremque rem deserunt reprehenderit blanditiis neque magnam. Quas, ab, esse harum optio itaque consectetur culpa, dignissimos cupiditate sapiente omnis quo et odit consequuntur nesciunt porro dolore enim quidem fuga sunt. Quis inventore enim, sed maiores soluta quae quasi reiciendis, laborum iure nihil, neque odio ipsa necessitatibus culpa assumenda cum repudiandae dignissimos cumque voluptate numquam? Ipsa!</ThemedText>
+                    </ThemedView>
+
+                </ThemedView>
+            
         </ThemedView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         position: "relative",
-        justifyContent: "center",
+        // justifyContent: "center",
+        height: '100%',
+        backgroundColor: "#fff",
     },
     map: {
         width: "100%",
-        height: "80%",
+        height: "83%",
+        // position: 'absolute',
+        // zIndex:2
     },
     helpButton: {
         position: "absolute",
@@ -191,7 +193,7 @@ const styles = StyleSheet.create({
         height: "100%",
     },
     paragraph: {
-        padding: 16,
+        padding: 4,
         fontSize: 15,
         textAlign: "center",
     },
@@ -207,6 +209,56 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     infoContainer: {
-        marginBottom: 20,
+        // marginBottom: 20,
+        
     },
+    infoWrapperMin:{
+        borderTopRightRadius: 40,
+        borderTopLeftRadius: 40,
+        position: 'relative',
+        backgroundColor: "#fff",
+        top: "-8%"
+    },
+    infoWrapperMax:{
+        // paddingTop: "100%",
+        // position: 'absolute',
+        // zIndex: 3,
+        borderTopRightRadius: 40,
+        borderTopLeftRadius: 40,
+        position: 'absolute',
+        marginTop: "30%",
+        backgroundColor: '#fff',
+        overflow: 'scroll',
+        // height: "50%",
+        // overflow: "scroll"
+        // height: 1
+        
+    },
+    infoButton:{
+        borderBottomRightRadius: 0,
+        borderBottomLeftRadius: 0,
+        borderTopRightRadius: 40,
+        borderTopLeftRadius: 40,
+        height: 20,
+        padding: 0,
+        // margin: 0,
+        backgroundColor: "#545454",
+        justifyContent: 'center',
+        alignItems: 'center'
+        // position: 'absolute',
+        // top: 0
+    },
+    infoMin:{
+        padding: 26,
+        alignItems: 'flex-start',
+        paddingRight: 45
+    },
+    infoMinWrapper: {
+        display: 'flex',
+        flexDirection: "row",
+        alignItems: "center",
+        // justifyContent: "space-between",
+        paddingHorizontal: 25,
+        paddingVertical: 10
+    }
 });
